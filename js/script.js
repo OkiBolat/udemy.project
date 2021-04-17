2da9bfa9a686cdbd2ddcc70dbd8babec181029c1
OkiBolat <okuevabubakar@gmail.com>
2021-04-17 13:05:03 +0300

    add practic for & while

--- js/script.js
+++ js/script.js
@@ -7,15 +7,50 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 }
-const lastFilm = prompt('Последний фильм?');
-const stars = prompt('Оценка?')
- personalMovieDB.movies[lastFilm] = stars;
+// const a = prompt('Последний фильм?'),
+//       b = prompt('Оценка?'),
+//       c = prompt('Последний фильм?'),
+//       d = prompt('Оценка?');
 
- if (4 == 9) {
-     console.log('Ok!');
- } else {
-     console.log('Error');
- }
+//  personalMovieDB.movies[a] = b;
+
+//  personalMovieDB.movies[c] = d;
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Последний фильм?'),
+          b = prompt('Оценка?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+    }else {
+        console.log('error');
+        i--;
+    }
+
+   
+}
+if (personalMovieDB.count < 10) {
+    console.log("not bad, not bad")
+
+}else if (personalMovieDB >= 10 && personalMovieDB <= 30){
+    console.log('you are good man')
+
+}else if(personalMovieDB >= 30) {
+    console.log('you kinoman')
+}else {
+    console.log('error')
+}
+
+console.log(personalMovieDB)
+
+//  if (4 == 9) {
+//      console.log('Ok!');
+//  } else {
+//      console.log('Error');
+//  }
+
 
 // if (num < 49) {
 //     console.log('error');
@@ -26,17 +61,42 @@ const stars = prompt('Оценка?')
 // }
 // (num === 50) ? console.log('ok') : console.log('error');
 
-const num = 50;
 
-switch (num) {
-    case 49:
-        console.log('no');
-        break;
-    case 100: 
-        console.log('no');
-        break;
-    default:
-        console.log('so bad man, so bad');
-        break;
 
-}
+// switch (num) {
+//     case 49:
+//         console.log('no');
+//         break;
+//     case 100: 
+//         console.log('no');
+//         break;
+//     default:
+//         console.log('so bad man, so bad');
+//         break;
+
+// }
+
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// };
+// // do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 55);
+// let num = 50;
+// for (let i = 1; i < 8; i++) {
+    // console.log(num);
+    // num++;
+// }
+// for (let i = 1; i < 10; i++) {
+//     if(i === 6) {
+//         continue;
+        
+//     }
+//     console.log(i)
+
+// }
+
