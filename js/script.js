447c536d59a4953b20bbd93bb55c6e2ee514404b
OkiBolat <okuevabubakar@gmail.com>
2021-04-17 12:14:22 +0300

    if else prac

--- js/script.js
+++ js/script.js
@@ -10,3 +10,33 @@ const personalMovieDB = {
 const lastFilm = prompt('Последний фильм?');
 const stars = prompt('Оценка?')
  personalMovieDB.movies[lastFilm] = stars;
+
+ if (4 == 9) {
+     console.log('Ok!');
+ } else {
+     console.log('Error');
+ }
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('mnogo');
+// }else {
+//     ('ok');
+// }
+// (num === 50) ? console.log('ok') : console.log('error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('no');
+        break;
+    case 100: 
+        console.log('no');
+        break;
+    default:
+        console.log('so bad man, so bad');
+        break;
+
+}
